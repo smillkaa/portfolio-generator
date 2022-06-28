bc024e6909d8252a16394be0fa710f8b7219766b
Seleste <104727784+smillkaa@users.noreply.github.com>
2022-06-27 23:02:25 -0700

    generated html page

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const printProfileData = profileDataArr => {
-  
-    profileDataArr.forEach(profileItem => console.log(profileItem))
-  };
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
