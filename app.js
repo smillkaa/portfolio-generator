f8c1411811b737cac77fa534dd9627e7cc10bb87
Seleste <104727784+smillkaa@users.noreply.github.com>
2022-06-29 23:32:47 -0700

    added inquirer

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const fs = require('fs');
+import inquirer from "inquirer";
 
-const generatePage = require('./src/page-template.js');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
 
-const profileDataArgs = process.argv.slice(2);
+  ])
+  .then(answers => console.log(answers))
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
